e76c6c32f5d03cf8f25782da910516ac203cdd43
chameliiia <chaemilee9012@gmail.com>
2021-02-18 12:34:19 +0900

    초기 commit

---
+++ New Unity Project/Assets/Script/Hole.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    public GameObject monster;
+
+    IEnumerator Start()
+    {
+        monster.SetActive(false);
+        while (true)
+        {
+            // 3초마다 두더지 active 상태로
+            yield return new WaitForSeconds(Random.Range(2.0f, 4.0f));
+            monster.SetActive(true);
+        }
+    }
+
+    public void OnClick()
+    {
+        // setActive true일때만 활성화
+        if (monster.activeSelf)
+        {
+            Score.value += 1;
+        }
+        monster.SetActive(false);
+    }
+}
---
+++ New Unity Project/Assets/Script/ScoreUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUI : MonoBehaviour
+{
+    public Text text;
+
+    private void Update()
+    {
+        text.text = $"점수: {Score.value}점";
+    }
+}
